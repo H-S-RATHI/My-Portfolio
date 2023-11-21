2154723889a91ae7a3adb0f117d1b1a95f2852aa
H-S-RATHI <goluu.000111@gmail.com>
2023-11-21 17:03:20 +0530

    Footer Added And All Document Formated

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -18,19 +18,19 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     //When we click on each nav__link, we remove the show-menu
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SHADOW HEADER ===============*/
-const shadowHeader = () =>{
+const shadowHeader = () => {
     const header = document.getElementById('header')
-    this.scrollY >=50?  header.classList.add('shadow-header')
-    :header.classList.remove('shadow-header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
 }
 
 window.addEventListener('scroll', shadowHeader)
@@ -39,36 +39,36 @@ window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
+    contactMessage = document.getElementById('contact-message')
 
 
-const sendEmail = (e) =>{
+const sendEmail = (e) => {
     e.preventDefault()
 
     //serviceID - templateId - #form - publicKey
-    emailjs.sendForm('service_e4id1c2','template_2xosk1f','#contact-form','ovuZPJnrkNamvBsXU')
-    .then(()=>{
-        //show sent message
-        contactMessage.textContent = 'Message sent successfully'
-        //remove message after five seconds
-        setTimeout(()=>{
-            contactMessage.textContent = ''
-        },5000)
+    emailjs.sendForm('service_e4id1c2', 'template_2xosk1f', '#contact-form', 'ovuZPJnrkNamvBsXU')
+        .then(() => {
+            //show sent message
+            contactMessage.textContent = 'Message sent successfully'
+            //remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
 
-        //clear Input Field
-        contactForm.reset();
-    }, () =>{
-        contactMessage.textContent = 'Message not sent (service error)'
-    })
+            //clear Input Field
+            contactForm.reset();
+        }, () => {
+            contactMessage.textContent = 'Message not sent (service error)'
+        })
 }
-contactForm.addEventListener('submit',sendEmail)
-/*=============== SHOW SCROLL UP ===============*/ 
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
